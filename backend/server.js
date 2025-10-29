d81e8c722dc33c095eb09c99c9deb671db8636fe
mattcurran00 <C23418016@mytudublin.ie>
2025-10-29 17:57:42 +0000

    Fixed the index showing ur entire C: drive

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,13 @@
-const express = require('express')';
+const express = require('express');
+const app = express();
+const port = 3000;
+
+//im pretty sure its express thats causing issues with the css scoping
+//have to serce the static files correctly
+app.use(express.static('frontend'));
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
+
 
